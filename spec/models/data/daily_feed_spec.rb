5ee3746006dfcd1f5b769c793362607bb8fee61a
Robert Williams <rob@r-williams.com>
2015-05-26 16:06:59 +0100

    Make .latest test a little more robust.
    If other db records creep in then the `.latest` record would not be the
    one that is created in the test.

--- spec/models/data/daily_feed_spec.rb
+++ spec/models/data/daily_feed_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Data::DailyFeed, type: :model do
   describe ".latest" do
     it "is the record with the most recently updated feed_updated_at column" do
-      new = Data::DailyFeed.create(feed_updated_at: 1.hour.ago)
+      new = Data::DailyFeed.create(feed_updated_at: Time.now)
       old = Data::DailyFeed.create(feed_updated_at: 1.week.ago)
 
       expect(Data::DailyFeed.latest).to eq(new)
