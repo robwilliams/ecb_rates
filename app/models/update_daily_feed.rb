5805edbea7d395e450ef9a1f4ae0cebc966eba13
Robert Williams <rob@r-williams.com>
2015-05-27 13:47:38 +0100

    Namespace incorrect on DailyFeed.

--- app/models/update_daily_feed.rb
+++ app/models/update_daily_feed.rb
@@ -1,7 +1,7 @@
 class UpdateDailyFeed
   def update
     ecb_feed = EcbApi.new.euroxref_daily
-    daily_feed = Data::DailyFeed.create(feed_updated_at: Time.zone.now)
+    daily_feed = Dao::DailyFeed.create(feed_updated_at: Time.zone.now)
 
     attributes = ecb_feed.map do |rate|
       {
